b206e2d75f70fcfb9b7db5f9e21e69dd43e76c0e
Edward <edward.gratefuldead1@gmail.com>
2023-11-09 17:11:17 -0600

    changes

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,6 +1,6 @@
-import { createUser, findUser } from "../db/Users";
 import express from "express";
 import { authentication, random } from "../helpers";
+import { createUser, findUser } from '../db/users';
 
 export const login = async (req: express.Request, res: express.Response) => {
   try {
